a0638f70aac3c2f8a9dda68938ed76c12b93cdca
Firebase Studio <noreply@firebase.studio>
2025-08-09 08:18:41 +0000

    Initialized workspace with Firebase Studio

---
+++ .idx/dev.nix
@@ -0,0 +1,52 @@
+{ pkgs, ... }:
+{
+  channel = "stable-23.11";
+
+  packages = [
+    pkgs.openjdk11
+    pkgs.nodejs_20
+    pkgs.mysql
+    pkgs.maven
+    pkgs.git
+    pkgs.nodePackages."@angular/cli"
+  ];
+
+  env = {
+    MYSQL_USER = "dev";
+    MYSQL_PASSWORD = "password";
+    MYSQL_DATABASE = "dev_db";
+    MYSQL_PORT = "3306";
+  };
+
+  services.mysql.enable = true;
+
+  idx = {
+    extensions = [
+      "angular.ng-template"
+      "vscjava.vscode-java-pack"
+      "redhat.java"
+    ];
+
+    workspace = {
+      onCreate = {
+        install = "./.idx/init-db.sh && (cd back/ && mvn clean install -DskipTests) & cd front/ && npm install";      
+      };
+      onStart = {
+        runServer = "(cd back/ && mvn spring-boot:run) & cd front/ && ng serve";      
+      };
+    };
+
+    previews = {
+      enable = true;
+      previews.web = {
+        manager = "web";
+        command = [
+          "sh"
+          "-c"
+          "cd front && ng serve --port 4200 --host 0.0.0.0"
+        ];
+      };
+    };
+
+  };
+}
